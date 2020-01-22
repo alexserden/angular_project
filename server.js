7e535556951e351e50291f273e477b9b5ae6c106
alexander <alexserden@gmail.com>
2020-01-22 22:27:35 +0200

    make it better

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/angular_project'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular_project/index.html'));
 });
 
 // default Heroku PORT
