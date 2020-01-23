bb27a9dd176187c8d0692f19915a5349e076e221
alexander <alexserden@gmail.com>
2020-01-23 12:37:38 +0200

    make it better

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/angular_project'));
+app.use(express.static(__dirname + '/dist'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular_project/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
+const { exec } = require('child_process');
 // default Heroku PORT
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3001);
+const command = `json-server --watch db.json --port ${3000}`;
+
+exec(command, (err, stdout, stderr) => {
+  if (err) {
+    console.log('Error running exec', err);
+    return;
+  }
+  console.log('stdout:', stdout);
+  console.log('stderr:', stderr);
+});
